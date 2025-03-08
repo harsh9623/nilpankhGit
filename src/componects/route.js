3c223c97cb538900adfd32b78e7c88018e8dc6f8
harshit <sonu10081987@gmail.com>
2025-03-08 16:09:36 +0530

    first commit

--- src/componects/route.js
+++ src/componects/route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import {HashRouter} from 'react-router-dom'
 // import Nav from './components/navigation';
 import Footer from './footer';
